169064ad3a7201249775e81c7a69d3840c563fc4
asonkes <audrey.sonkes@gmail.com>
2024-04-26 08:41:06 +0200

    page home terminée

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,27 @@
 import './styles/index.scss';
 
+/** MENU BURGER - Transormation burger en croix au 'click' **/
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+})
+
+/**  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
