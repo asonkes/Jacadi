aff8e9a6d05a2f91d1c5e9d587d5d7ae4e967638
asonkes <audrey.sonkes@gmail.com>
2024-06-12 20:49:52 +0200

    admin - formulaire ajout de produits

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,17 +18,50 @@ class ProductsController extends AbstractController
 {
     #[Route('/produits', name: 'products')]
     #[IsGranted('ROLE_ADMIN_CLIENT')]
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
         return $this->render('admin/products/index.html.twig');
     }
 
     #[Route('/produits/ajout', name: 'add')]
     #[IsGranted('ROLE_ADMIN_CLIENT')]
-    public function add(): Response
+    public function add(Products $products, Request $request, EntityManagerInterface $entityManagerInterface, SluggerInterface $slugger): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN_CLIENT');
+
+        // On créé un "nouveau produit"
+        $product = new $products();
+
+        // On créé le formulaire
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        // On traite la requête du formulaire
+        $productForm->handleRequest($request);
+
+        // On vérifie si le formulaire est soumis et valide
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // On génère le slug
+            $slug = $slugger->slug($product->getName())->lower();
+            $product->setSlug($slug);
+
+            // On arrondi le prix 
+            $prix = $product->getPrice();
+            $product->setPrice($prix);
+
+            // On stocke
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            $this->addFlash('success', 'Produit ajouté avec succès');
+
+            // On redirige
+
+            return $this->redirectToRoute('admin_add');
+        }
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
 
     #[Route('/produits/edition/{id}', name: 'edit')]
---
+++ src/Form/ProductsFormType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categories;
+use App\Entity\Products;
+use App\Repository\CategoriesRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', options: [
+                'label' => 'Nom'
+            ])
+            ->add('price', options: [
+                'label' => 'Prix'
+            ])
+            ->add('image')
+            ->add('content', options: [
+                'label' => 'Description'
+            ])
+            ->add('composition')
+            ->add('advices', options: [
+                'label' => "Conseils d'entretien"
+            ])
+            ->add('eco', options: [
+                'label' => 'Articles Responsables'
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'group_by' => 'parent.name',
+                'query_builder' => function (CategoriesRepository $cr) {
+                    return $cr->createQueryBuilder('c')
+                        ->where('c.parent IS NOT NULL')
+                        ->orderBy('c.name', 'ASC');
+                }
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
